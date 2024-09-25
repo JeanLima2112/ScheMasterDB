2a598a589059b80f1cf825ffd26cdca08662c32c
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-09-25 15:07:35 -0300

    add a inicial workspace

--- schemasterdb_Front_End/src/assets/pages/Router.tsx
+++ schemasterdb_Front_End/src/assets/pages/Router.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./home/Home";
+import Home from "./Home/Home";
+import WorkSpace from "./workspace/WorkSpace";
+
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="workspace" element={<WorkSpace />} />
     </Routes>
   );
 };
