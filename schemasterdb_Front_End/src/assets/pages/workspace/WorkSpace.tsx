ab1f22459e968ef69f3a41f38629ffde0394a428
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-09-26 23:20:07 -0300

    initial desing for workspace

--- schemasterdb_Front_End/src/assets/pages/workspace/WorkSpace.tsx
+++ schemasterdb_Front_End/src/assets/pages/workspace/WorkSpace.tsx
@@ -1,45 +1,94 @@
-import React, { useCallback } from 'react';
 import {
-  ReactFlow,
-  MiniMap,
-  Controls,
-  Background,
-  useNodesState,
-  useEdgesState,
+  Button,
+  Drawer,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  Flex,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import {
   addEdge,
-} from '@xyflow/react';
-import {Flex} from '@chakra-ui/react'
- 
-import '@xyflow/react/dist/style.css';
- 
+  Background,
+  Controls,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+} from "@xyflow/react";
+import { useCallback } from "react";
+
+import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
+
+import "@xyflow/react/dist/style.css";
+import Header from "../../../components/header/Header";
+
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
 export default function WorkSpace() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges],
+    [setEdges]
   );
- 
+
   return (
-    <Flex w='100vw' h='100vh'>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      >
-        <Controls />
-        <MiniMap />
-        <Background variant="dots" gap={12} size={1} />
-      </ReactFlow>
+    <Flex w="100dvw" h="100dvh" direction="column">
+      <Header />
+      <Flex w="100dvw" h="94dvh">
+        <Flex
+          w="15dvw"
+          h="94dvh"
+          border="1px solid black"
+          bg="#f6f6f6"
+          borderRightRadius="2rem"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text>Tools</Text>
+        </Flex>
+        <Flex w="85dvw" h="94dvh">
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            attributionPosition="bottom-right"
+          >
+            <Controls />
+            <Background gap={12} size={1} />
+          </ReactFlow>
+          <Flex></Flex>
+          <Button
+            position="absolute"
+            top="10%"
+            right="0%"
+            onClick={onOpen}
+            bg="gray.600"
+            color="white"
+            borderRadius="0 5px 5px 0"
+          >
+            <MdOutlineKeyboardDoubleArrowLeft />
+          </Button>
+          <Drawer size="md" placement="right" onClose={onClose} isOpen={isOpen}>
+            <DrawerContent >
+              <DrawerCloseButton />
+              <Flex justifyContent="center">
+                <DrawerHeader>Edit Atributes</DrawerHeader>
+              </Flex>
+            </DrawerContent>
+          </Drawer>
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
---
+++ schemasterdb_Front_End/src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Header(){
+    return(
+        <Flex w='100%' h='6dvh' border='1px solid ' bg='#f6f6f6' borderBottomRightRadius='.8rem' justifyContent='center' alignItems='center'>
+            <Text>Header</Text>
+        </Flex>
+    )
+} 
