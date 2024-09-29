62be0b8ce53c2e37c7a2f65d1b17718d1c9bed3b
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-09-29 15:01:48 -0300

    connection relation style

--- schemasterdb_Front_End/src/assets/pages/workspace/components/edges/Relation.tsx
+++ schemasterdb_Front_End/src/assets/pages/workspace/components/edges/Relation.tsx
@@ -1,13 +1,29 @@
-import { Flex, Text } from "@chakra-ui/react";
 import { BaseEdge, EdgeProps, getStraightPath } from "@xyflow/react";
+import React from "react";
 
-const CustomEdge = ({id, sourceX, sourceY, targetX, targetY}: EdgeProps) => {
+const CustomEdge = ({ id, sourceX, sourceY, targetX, targetY, label }: EdgeProps) => {
   const [edgePath] = getStraightPath({
     sourceX,
     sourceY,
     targetX,
     targetY,
   });
+
+  const midX = (sourceX + targetX) / 2;
+  const midY = (sourceY + targetY) / 2;
+
+
+
+  const diamondWidth = 150; 
+  const diamondHeight = 100; 
+
+  const diamondPath = `
+    M ${midX},${midY - diamondHeight / 2} 
+    L ${midX + diamondWidth / 2},${midY} 
+    L ${midX},${midY + diamondHeight / 2} 
+    L ${midX - diamondWidth / 2},${midY} 
+    Z
+  `;
 
   return (
     <>
@@ -17,6 +33,23 @@ const CustomEdge = ({id, sourceX, sourceY, targetX, targetY}: EdgeProps) => {
         style={{ stroke: "#000", strokeWidth: 2 }}
       />
 
+      <path
+        d={diamondPath}
+        fill="#fff" 
+        stroke="#000"
+        strokeWidth={2} 
+      />
+
+  
+      <text
+        x={midX}
+        y={midY}
+        textAnchor="middle"
+        alignmentBaseline="central"
+        style={{ fontSize: 18, fill: "#000", fontWeight: 'bold'}}
+      >
+        Rel
+      </text>
     </>
   );
 };
